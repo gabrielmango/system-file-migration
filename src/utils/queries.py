0d228c4d8387273957738d8f2011cb93e3b5d2cc
gabrielhmango <gabrielhmango@gmail.com>
2025-07-15 14:56:48 -0300

    fix(utils): fix sql query

--- src/utils/queries.py
+++ src/utils/queries.py
@@ -1,11 +1,8 @@
 DATA_CESV_QUERY = """
 SELECT 
     t1.co_uuid_2,
-    t1.co_uuid AS uuid_candidatura,
-    t2.co_uuid AS uuid_processo 
+    t1.co_uuid AS uuid_candidatura
 FROM cesv.tb_candidatura t1
-LEFT JOIN cesv.tb_processo_seletivo t2
-    ON t1.co_processo_seletivo = t2.co_seq_processo_seletivo
 ORDER BY t1.co_seq_candidatura DESC;
 """
 
