67bb7b09c19a25735c1287a87b3665541144f6cc
gabrielhmango <gabrielhmango@gmail.com>
2025-07-15 12:00:08 -0300

    feat(pipeline): add fileserver acess

--- src/pipelines/cesv_file_migration_pipeline.py
+++ src/pipelines/cesv_file_migration_pipeline.py
@@ -1,7 +1,8 @@
 from src.core.connections.postgres_connection import PostgresConnection
 from src.core.extractors.postgres_extractor import PostgresExtractor
 from src.pipelines.base_pipeline import BasePipeline
-from src.utils.config.postgres_acess import CesvPostgresAcess
+from src.utils.config.postgres_acess import (CesvPostgresAcess,
+                                             FileserverPostgresAcess)
 from src.utils.queries import DATA_CESV_QUERY, DATA_FILESERVER_QUERY
 
 
@@ -9,6 +10,7 @@ class CesvFileMigration(BasePipeline):
     def __init__(self):
         super().__init__(name='cesv_file_migration')
         self.__acess_cesv_postgres = CesvPostgresAcess().as_dict()
+        self.__acess_fileserver_postgres = FileserverPostgresAcess().as_dict()
 
     def run(self):
         data = self._extract()
@@ -32,7 +34,7 @@ class CesvFileMigration(BasePipeline):
 
     def _extract_data_from_fileserver(self):
         postgres_conn = PostgresConnection(
-            self.__acess_cesv_postgres.get('FILESERVER_POSTGRES_PROD')
+            self.__acess_fileserver_postgres.get('FILESERVER_POSTGRES_PROD')
         )
         extractor = PostgresExtractor(db_connection=postgres_conn)
         return extractor.extract(query=DATA_FILESERVER_QUERY)
