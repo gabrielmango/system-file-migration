8dc94ca7d00d3cc3f734479e8587ac121f8a095e
gabrielhmango <gabrielhmango@gmail.com>
2025-07-15 12:11:31 -0300

    feat(transformer): add class PandasTransformer

---
+++ src/core/transformers/pandas_transformer.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+from src.core.transformers.base_transformer import BaseTransformer
+
+
+class PandasTransformer(BaseTransformer):
+    def transform(self, data, *args, **kwargs):
+        column = kwargs.get('column')
+        return self._merge_data(data, column)
+
+    def _merge_data(self, data, column):
+        dfs = [pd.DataFrame(lines) for lines in data.values()]
+
+        return pd.merge(dfs[0], dfs[1], on=column, how='left').to_dict()
